6a2b240bb3d2cdbfb06054a41467614e7d7f4a01
Jeferson Coutinho <jefissuu@gmail.com>
2022-07-30 19:31:00 -0300

    Added animations between screens and changed app icon

--- app/src/main/java/com/jefisu/hashgenerator/MainActivity.kt
+++ app/src/main/java/com/jefisu/hashgenerator/MainActivity.kt
@@ -25,12 +25,15 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
 import com.jefisu.hashgenerator.destinations.HashGeneratedScreenDestination
 import com.jefisu.hashgenerator.ui.theme.AzureRadiance
 import com.jefisu.hashgenerator.ui.theme.Downriver
 import com.jefisu.hashgenerator.ui.theme.Midnight
 import com.jefisu.hashgenerator.ui.theme.Turquoise
 import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.animations.defaults.RootNavGraphDefaultAnimations
+import com.ramcosta.composedestinations.animations.rememberAnimatedNavHostEngine
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
@@ -38,16 +41,27 @@ import com.ramcosta.composedestinations.navigation.dependency
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+@ExperimentalMaterialNavigationApi
 @ExperimentalMaterial3Api
+@ExperimentalAnimationApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val snackBarHostState = remember { SnackbarHostState() }
+            val navHostEngine = rememberAnimatedNavHostEngine(
+                rootDefaultAnimations = RootNavGraphDefaultAnimations(
+                    enterTransition = { fadeIn(animationSpec = tween(700)) },
+                    exitTransition = { fadeOut(animationSpec = tween(700)) },
+                    popEnterTransition = { slideInVertically() + fadeIn(animationSpec = tween(700)) },
+                )
+            )
             Scaffold(
-                snackbarHost = { SnackbarHost(hostState = snackBarHostState) }
+                snackbarHost = { SnackbarHost(hostState = snackBarHostState) },
+                containerColor = Midnight
             ) { innerPadding ->
                 DestinationsNavHost(
+                    engine = navHostEngine,
                     navGraph = NavGraphs.root,
                     dependenciesContainerBuilder = {
                         dependency(snackBarHostState)
@@ -70,10 +84,19 @@ fun MainScreen(
     var text by remember { mutableStateOf("") }
     var selectedAlgorithm by remember { mutableStateOf(algorithms[0]) }
     val scope = rememberCoroutineScope()
+    var isNavigating by remember { mutableStateOf(false) }
+
+    LaunchedEffect(key1 = isNavigating) {
+        if (isNavigating) {
+            delay(500)
+            navigator.navigate(
+                HashGeneratedScreenDestination(NavArguments(text, selectedAlgorithm))
+            )
+        }
+    }
 
     Box(
         modifier = Modifier
-            .background(Midnight)
             .fillMaxSize()
             .padding(24.dp)
     ) {
@@ -81,87 +104,113 @@ fun MainScreen(
             verticalArrangement = Arrangement.spacedBy(16.dp),
             modifier = Modifier.align(Alignment.Center)
         ) {
-            Text(
-                text = "HASH \nGENERATOR",
-                color = Color.White,
-                fontWeight = FontWeight.Bold,
-                style = MaterialTheme.typography.displaySmall,
-                textAlign = TextAlign.Center,
+            AnimatedVisibility(
+                visible = !isNavigating,
+                enter = fadeIn(animationSpec = tween(500)),
+                exit = fadeOut(animationSpec = tween(500)),
                 modifier = Modifier.align(Alignment.CenterHorizontally)
-            )
-            ExposedDropDown(
-                algorithms = algorithms.asList(),
-                selectedAlgorithm = selectedAlgorithm,
-                onClickSelectedAlgorithm = { selectedAlgorithm = it },
-                modifier = Modifier.fillMaxWidth()
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(150.dp)
-                    .clip(MaterialTheme.shapes.extraSmall)
-                    .background(Downriver)
             ) {
-                TextField(
-                    value = text,
-                    onValueChange = { text = it },
-                    textStyle = MaterialTheme.typography.bodyLarge,
-                    maxLines = 7,
-                    placeholder = { Text(text = "Text here...") },
-                    colors = TextFieldDefaults.textFieldColors(
-                        containerColor = Color.Transparent,
-                        focusedIndicatorColor = Color.Transparent,
-                        unfocusedIndicatorColor = Color.Transparent,
-                        placeholderColor = Color.LightGray,
-                        textColor = Color.White
-                    )
+                Text(
+                    text = "HASH \nGENERATOR",
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.displaySmall,
+                    textAlign = TextAlign.Center
                 )
-                if (text.isNotBlank()) {
-                    IconButton(
-                        onClick = { text = "" },
-                        modifier = Modifier.align(Alignment.BottomEnd),
-                        colors = IconButtonDefaults.iconButtonColors(
-                            contentColor = Color.White
+            }
+            AnimatedVisibility(
+                visible = !isNavigating,
+                enter = slideInHorizontally(animationSpec = tween(500)) + fadeIn(),
+                exit = slideOutHorizontally(animationSpec = tween(500)) + fadeOut()
+            ) {
+                ExposedDropDown(
+                    algorithms = algorithms.asList(),
+                    selectedAlgorithm = selectedAlgorithm,
+                    onClickSelectedAlgorithm = { selectedAlgorithm = it },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+            AnimatedVisibility(
+                visible = !isNavigating,
+                enter = slideInHorizontally(
+                    animationSpec = tween(500),
+                    initialOffsetX = { it }) + fadeIn(),
+                exit = slideOutHorizontally(
+                    animationSpec = tween(500),
+                    targetOffsetX = { it }) + fadeOut()
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(150.dp)
+                        .clip(MaterialTheme.shapes.extraSmall)
+                        .background(Downriver)
+                ) {
+                    TextField(
+                        value = text,
+                        onValueChange = { text = it },
+                        textStyle = MaterialTheme.typography.bodyLarge,
+                        maxLines = 7,
+                        placeholder = { Text(text = "Text here...") },
+                        colors = TextFieldDefaults.textFieldColors(
+                            containerColor = Color.Transparent,
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            placeholderColor = Color.LightGray,
+                            textColor = Color.White
                         )
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Clear,
-                            contentDescription = null
-                        )
+                    )
+                    if (text.isNotBlank()) {
+                        IconButton(
+                            onClick = { text = "" },
+                            modifier = Modifier.align(Alignment.BottomEnd),
+                            colors = IconButtonDefaults.iconButtonColors(
+                                contentColor = Color.White
+                            )
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Clear,
+                                contentDescription = null
+                            )
+                        }
                     }
                 }
             }
             Spacer(modifier = Modifier.height(48.dp))
         }
-        Button(
-            onClick = {
-                if (text.isBlank() || text.isEmpty()) {
-                    scope.launch {
-                        snackBarHostState.showSnackbar(
-                            message = "Could not generate hash. Please try again.",
-                            actionLabel = "OK"
-                        )
-                    }
-                    return@Button
-                }
-                navigator.navigate(
-                    HashGeneratedScreenDestination(NavArguments(text, selectedAlgorithm))
-                )
-            },
-            shape = MaterialTheme.shapes.extraSmall,
-            colors = ButtonDefaults.buttonColors(
-                containerColor = AzureRadiance,
-                contentColor = Color.White
-            ),
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(50.dp)
-                .align(Alignment.BottomCenter)
+        AnimatedVisibility(
+            visible = !isNavigating,
+            enter = fadeIn(animationSpec = tween(500)),
+            exit = fadeOut(animationSpec = tween(500)),
+            modifier = Modifier.align(Alignment.BottomCenter)
         ) {
-            Text(
-                text = "GENERATE",
-                fontSize = 18.sp
-            )
+            Button(
+                onClick = {
+                    if (text.isBlank() || text.isEmpty()) {
+                        scope.launch {
+                            snackBarHostState.showSnackbar(
+                                message = "Could not generate hash. Please try again.",
+                                actionLabel = "OK"
+                            )
+                        }
+                        return@Button
+                    }
+                    isNavigating = true
+                },
+                shape = MaterialTheme.shapes.extraSmall,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = AzureRadiance,
+                    contentColor = Color.White
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+            ) {
+                Text(
+                    text = "GENERATE",
+                    fontSize = 18.sp
+                )
+            }
         }
     }
 }
@@ -183,9 +232,7 @@ fun HashGeneratedScreen(
     }
 
     Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Midnight)
+        modifier = Modifier.fillMaxSize()
     ) {
         IconButton(
             onClick = navigator::navigateUp,
